fe0491ec9db958e79ca2e1d496a9117162f7d8fa
Jonathan Rhone <jonathanrhone@Jonathans-Air.attlocal.net>
2024-08-30 01:47:32 -0400

    Cleanup, refactor, try to normalize speed on Delta Speed

--- DeltaSpeed.js
+++ DeltaSpeed.js
@@ -6,6 +6,7 @@ class DeltaSpeed {
         this.deltaStart = null;
         this.lastUpdateTime = null;
         this.deltaSpeed = 0;
+        this.maxSpeed = 1; // Initialize max speed to prevent division by zero
         this.lastSpeedCalculationTime = null; // Track when the last delta speed was calculated
         this.initialDraw = false; // Flag to handle the initial zero data point
     }
@@ -19,6 +20,7 @@ class DeltaSpeed {
             this.deltaStart = currentDelta;
             this.lastUpdateTime = currentTime;
             this.lastSpeedCalculationTime = currentTime;
+            return 0;
         }
 
         const elapsedTime = currentTime - this.lastUpdateTime;
@@ -27,41 +29,41 @@ class DeltaSpeed {
             const deltaChange = currentDelta - this.deltaStart;
             this.deltaSpeed = deltaChange / (elapsedTime / 1000);
 
-            // Reset for the next period
+
+            // Update maxSpeed if the current speed is higher
+            if (Math.abs(this.deltaSpeed) > this.maxSpeed) {
+                this.maxSpeed = Math.abs(this.deltaSpeed);
+            }
+
+            // Normalize the deltaSpeed
+            const normalizedSpeed = this.deltaSpeed / this.maxSpeed;
+
             this.deltaStart = currentDelta;
             this.lastUpdateTime = currentTime;
-        }
 
-        // // Plot the initial zero data point as the "previous" bar
-        if (!this.initialDraw) {
-            this.initialDraw = true;
-            return 0;
-        }
-
-        // Only return deltaSpeed at 1-second intervals
-        if (currentTime - this.lastSpeedCalculationTime >= periodInMillis) {
-            this.lastSpeedCalculationTime = currentTime;
-            return this.deltaSpeed;
+            return normalizedSpeed; // Return normalized speed
         }
 
         return null; // Don't plot anything until the next interval
-        // return this.sma(d.value());
     }
 }
 
-function dnaLikePlotter(canvas, calculatorInstance, history) {
-    for(let i=0; i<history.data.length; ++i) {
-        const item = history.get(i);
-        const x = p.x.get(item);
-        canvas.drawLine(p.offset(x, item), {});
-    }
-}
+// function dnaLikePlotter(canvas, calculatorInstance, history) {
+//     for(let i=0; i<history.data.length; ++i) {
+//         const item = history.get(i);
+//         const x = p.x.get(item);
+//         canvas.drawLine(p.offset(x, item), {});
+//     }
+// }
+
+// TODO can we make the avg speed of the periods that comprise the candle the value for the final plot?
+// TODO how to debug this? Why it goes from 1 to 0.. feels like the initial max delta might not be accurate
 
 module.exports = {
     name: "DeltaSpeed",
     description: /*i18n*/ "Delta Speed",
     calculator: DeltaSpeed,
-    plotter: predef.plotters.custom(dnaLikePlotter),
+    // plotter: predef.plotters.custom(dnaLikePlotter),
     params: {
         period: predef.paramSpecs.period(1)
     },
